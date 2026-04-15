c9eae97a4d3ba0a0897aced150da2600794762a5
mthgp26 <199875470+mthgp26@users.noreply.github.com>
2026-04-15 19:25:58 +0800

    fixing the bug and execution breakpoints

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -33,22 +33,35 @@ public class TestBuggyAList {
     @Test
     public void randomizedTest(){
         AListNoResizing<Integer> L = new AListNoResizing<>();
-        int N = 500;
+        BuggyAList<Integer> B = new BuggyAList<>();
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
+                B.addLast(randVal);
                 L.addLast(randVal);
                 System.out.println("addLast(" + randVal + ")");
             } else if (operationNumber == 1) {
                 // size
                 int size = L.size();
+                int Bsize = B.size();
                 System.out.println("size: " + size);
+                System.out.println("Bsize:" + B.size());
+                assertEquals(size, Bsize);
             } else if (operationNumber == 2 && L.size() > 0) {
-                System.out.println("getLast(): " + L.getLast());
+                int expected = L.getLast();
+                int output = B.getLast();
+                System.out.println("L.getLast(): " + expected);
+                System.out.println("B.getLast(): " + output);
+                assertEquals(expected, output);
             } else if (operationNumber == 3 && L.size() > 0) {
-                System.out.println("removeLast():" + L.removeLast());
+                int expected = L.removeLast();
+                int output = B.removeLast();
+                System.out.println("L.removeLast():" + expected);
+                System.out.println("B.removeLast():" + output);
+                assertEquals(expected, output);
             }
         }
     }
